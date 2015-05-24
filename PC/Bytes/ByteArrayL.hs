f8fce1aeda0d877c43f053ab306172dd3f5c394a
Vincent Hanquez <vincent@snarc.org>
2015-05-24 21:51:28 +0100

    update license

--- PC/Bytes/ByteArrayL.hs
+++ PC/Bytes/ByteArrayL.hs
@@ -1,11 +1,27 @@
--- Copyright (c) 2013-2015 PivotCloud, Inc. All Rights Reserved.
+-- Copyright (c) 2013-2015 PivotCloud, Inc.
 --
--- NOTICE: The dissemination, reproduction, or copying of this file and the
--- information contained herein, in any medium, is strictly forbidden.
+-- PC.Bytes.ByteArrayL
 --
--- The intellectual property and technical concepts contained herein are
--- proprietary to PivotCloud and are protected by U.S. and Foreign law.
-
+-- Please feel free to contact us at licensing@pivotmail.com with any
+-- contributions, additions, or other feedback; we would love to hear from
+-- you.
+--
+-- Licensed under the Apache License, Version 2.0 (the "License"); you may
+-- not use this file except in compliance with the License. You may obtain a
+-- copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+-- WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+-- License for the specific language governing permissions and limitations
+-- under the License.
+--
+-- |
+-- Module      : PC.Bytes.ByteArrayL
+-- Copyright   : (c) 2013-2015 PivotCloud, Inc
+-- License     : Apache-2, see LICENSE file of the package
+-- Maintainer  : licensing@pivotmail.com
+--
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE TypeFamilies #-}
@@ -13,7 +29,6 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE ConstraintKinds #-}
-
 module PC.Bytes.ByteArrayL
 ( ByteArrayL(..)
 , BackendByteArrayL
